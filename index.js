87dd33d94c637ad32d6bccb879034565b89fb15f
mareeswaran <mareescse104@gmail.com>
2024-07-30 22:40:02 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+roundsSelect = document.querySelector('#roundsSelect')
+roundSelectContainer = document.querySelector('#roundSelectContainer')
+errorMessage = document.querySelector('#errorMessage')
+popup = document.querySelector('#popup')
+closeButton = document.querySelector('#closeButton')
+
+playButton = document.querySelector('#playButton')
+nextRoundButton = document.querySelector('#nextRoundButton')
+initButton = document.querySelector('#initButton')
+
+gameControls = document.querySelectorAll('.gameControls')
+userControls = document.querySelectorAll('.userControls')
+computerControls = document.querySelectorAll('.computerControls')
+
+roundID = document.querySelector('#roundID')
+roundSpan = roundID.querySelector('span')
+
+countDown = document.querySelector('#countDown')
+
+userScore = document.querySelector('#countDown')
